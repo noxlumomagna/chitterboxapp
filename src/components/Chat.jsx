ca73026366c181c431479de0fa3cd30198abd2a5
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-10-03 17:12:41 -0400

    Add files via upload

---
+++ src/components/Chat.jsx
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import Vid from "../images/cam.png";
+import Add from "../images/add.png";
+import More from "../images/more.png";
+import Messages from "./Messages";
+import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
+
+const Chat = () => {
+  const { data } = useContext(ChatContext);
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>{data.user?.displayName}</span>
+        <div className="chatIcons">
+          <img src={Vid} alt=""></img>
+          <img src={Add} alt=""></img>
+          <img src={More} alt=""></img>
+        </div>
+      </div>
+      <Messages />
+      <Input />
+    </div>
+  );
+};
+
+export default Chat;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,42 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { auth } from "../firebase";
+
+const Login = () => {
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">ChitterBox</span>
+        <span className="title">Login</span>
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="E-mail address" />
+          <input type="password" placeholder="password" />
+          <button>Log In</button>
+          {err && <span>Username or password entered incorrectly.</span>}
+        </form>
+        <p>
+          Don't have an account? <Link to="/register">Register</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
